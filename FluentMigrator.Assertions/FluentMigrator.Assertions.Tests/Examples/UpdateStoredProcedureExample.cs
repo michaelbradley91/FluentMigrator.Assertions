bf152fd698bc9631f56f63cc3352c8d5f29373df
Michael Bradley <michael.bradley@hotmail.co.uk>
2016-05-27 00:48:15 +0100

    Adding stored procedure example.

---
+++ FluentMigrator.Assertions/FluentMigrator.Assertions.Tests/Examples/UpdateStoredProcedureExample.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator.Assertions.Migrations;
+
+namespace FluentMigrator.Assertions.Tests.Examples
+{
+    public class UpdateStoredProcedureExample : MigrationWithAssertions
+    {
+        private const string ProcedureName = "MyProcedure";
+        private const string NewProcedureSql = "NewProcedure.sql";
+        private const string OldProcedureSql = "OldProcedure.sql";
+
+        public override void Up()
+        {
+            Assert.StoredProcedureExists(ProcedureName).WithDefinition(OldProcedureSql);
+            Execute.Sql($"DROP PROCEDURE {ProcedureName}");
+            Execute.EmbeddedScript(NewProcedureSql);
+        }
+
+        public override void Down()
+        {
+            Assert.StoredProcedureExists(ProcedureName).WithDefinition(NewProcedureSql);
+            Execute.Sql($"DROP PROCEDURE {ProcedureName}");
+            Execute.EmbeddedScript(OldProcedureSql);
+        }
+    }
+}
