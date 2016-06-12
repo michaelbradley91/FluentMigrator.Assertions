88990d4cb6d15398e6853e681c67bf48d51f2115
Michael Bradley <michael.bradley@hotmail.co.uk>
2016-06-12 22:16:32 +0100

    Fixing the SQL helper to remove whitespace and SQL comments.

--- FluentMigrator.Assertions/FluentMigrator.Assertions/Helpers/SqlHelpers.cs
+++ FluentMigrator.Assertions/FluentMigrator.Assertions/Helpers/SqlHelpers.cs
@@ -1,4 +1,6 @@
-﻿namespace FluentMigrator.Assertions.Helpers
+﻿using System;
+
+namespace FluentMigrator.Assertions.Helpers
 {
     public static class SqlHelpers
     {
@@ -10,6 +12,30 @@
         public static string CreateRemoveNewLinesSql(string sqlString)
         {
             return $"REPLACE(REPLACE({sqlString}, CHAR(10), ''), CHAR(13), '')";
+        }
+
+        public static string RemoveCommentsAndWhiteSpace(out string variableName, string str)
+        {
+            variableName = "X" + Guid.NewGuid().ToString("N");
+            return $@"
+DECLARE @CodeBlockStart{variableName} int, @CodeBlockEnd{variableName} int, @{variableName} varchar(max)
+SET @CodeBlockStart{variableName} = PATINDEX('%/*%', @{variableName})
+
+WHILE @CodeBlockStart{variableName} > 0
+BEGIN
+    SET @CodeBlockEnd{variableName} = PATINDEX('%*/%', @{variableName})
+    SET @{variableName} = LEFT(@{variableName}, @CodeBlockStart{variableName} - 1) + RIGHT(@{variableName}, LEN(@{variableName}) - (@CodeBlockEnd{variableName} + 1))
+    SET @CodeBlockStart{variableName} = PATINDEX('%/*%', @{variableName})
+END
+
+DECLARE @DoubleDashStart{variableName} int, @DoubleDashLineEnd{variableName} int
+SET @DoubleDashStart{variableName} = PATINDEX('%--%', @{variableName})
+WHILE @DoubleDashStart{variableName} > 0
+BEGIN
+    SET @DoubleDashLineEnd{variableName} = PATINDEX('%' + CHAR(13) + CHAR(10) + '%', RIGHT(@{variableName}, LEN(@{variableName}) - (@DoubleDashStart{variableName}))) + @DoubleDashStart{variableName}
+    SET @{variableName} = LEFT(@{variableName}, @DoubleDashStart{variableName} - 1) + RIGHT(@{variableName}, LEN(@{variableName}) - @DoubleDashLineEnd{variableName})
+    SET @DoubleDashStart{variableName} = PATINDEX('%--%', @{variableName})
+END";
         }
     }
 }
