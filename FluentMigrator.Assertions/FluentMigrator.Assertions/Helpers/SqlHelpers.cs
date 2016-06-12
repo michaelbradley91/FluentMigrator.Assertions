c9cd49743bc48c2012b27bd8ee383cd7b5595c45
Michael Bradley <michael.bradley@hotmail.co.uk>
2016-06-12 22:24:03 +0100

    Including whitespace removal this time!

--- FluentMigrator.Assertions/FluentMigrator.Assertions/Helpers/SqlHelpers.cs
+++ FluentMigrator.Assertions/FluentMigrator.Assertions/Helpers/SqlHelpers.cs
@@ -31,7 +31,23 @@ BEGIN
     SET @DoubleDashLineEnd{variableName} = PATINDEX('%' + CHAR(13) + CHAR(10) + '%', RIGHT(@{variableName}, LEN(@{variableName}) - (@DoubleDashStart{variableName}))) + @DoubleDashStart{variableName}
     SET @{variableName} = LEFT(@{variableName}, @DoubleDashStart{variableName} - 1) + RIGHT(@{variableName}, LEN(@{variableName}) - @DoubleDashLineEnd{variableName})
     SET @DoubleDashStart{variableName} = PATINDEX('%--%', @{variableName})
-END";
+END
+--Horizontal Tab
+SET @{variableName} = Replace(@{variableName},CHAR(9),'');
+--Line Feed
+SET @{variableName} = Replace(@{variableName},CHAR(10),'');
+--Vertical Tab
+SET @{variableName} = Replace(@{variableName},CHAR(11),'');
+--Form Feed
+SET @{variableName} = Replace(@{variableName},CHAR(12),'');
+--Carriage Return
+SET @{variableName} = Replace(@{variableName},CHAR(13),'');
+--Column Break
+SET @{variableName} = Replace(@{variableName},CHAR(14),'');
+--Non-breaking space
+SET @{variableName} = Replace(@{variableName},CHAR(160),'');
+SET @{variableName} = LTRIM(RTRIM(@{variableName}));
+";
         }
     }
 }
